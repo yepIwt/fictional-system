ced8e8877db430125712d36d2ced092aea7f864a
Nikita Sergievsky <ReetRay@protonmail.com>
2021-09-28 22:47:53 +0300

    Добавил функцию ЧБ для фоток

--- src/pics.h
+++ src/pics.h
@@ -18,6 +18,25 @@ QImage crop_picture(QImage picture_obj, int x1, int y1, int x2, int y2){
     return cropped_picture_obj;
 };
 
+QImage make_bw(QImage picture_obj){
+    // QImage bw_pic = picture_obj;
+    // bw_pic.applyColorTransform(QImage::Format_Grayscale8);
+    // return bw_pic;
+    QSize sizeImage = picture_obj.size();
+    int width = sizeImage.width(), height = sizeImage.height();
+    QRgb color;
+    int value;
+    for (int f1=0; f1<width; f1++) {
+        for (int f2=0; f2<height; f2++) {
+            color = picture_obj.pixel(f1,f2);
+            int gray = (qRed(color) + qGreen(color) + qBlue(color))/3;
+            picture_obj.setPixel(f1, f2, qRgb(gray, gray, gray));
+        }
+    };
+    return picture_obj;
+
+};
+
 void save_image(QImage picture_obj, std::string filename){
     QString qfilename = filename.c_str();
     picture_obj.save(qfilename);

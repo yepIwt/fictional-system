834b0c2700b648b0c120ac8efa1b0ed717ed7f2e
Вячеслав <bg1622h@gmail.com>
2021-10-29 18:36:42 +0300

    Создаю прямоугольник в чисто белом изображении в крайне левом верхнем углу (x,y). Размерами w,h в изображении размерами (maxx;maxh)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <QtWidgets>
 #include <QApplication>
 #include <QtGui>
-
+#include <iostream>
 #include "src/pics.h"
 #include "src/lbp.h"
-
+#include "net/gist.h"
+#include "net/shab.h"
 const int P = 8;
 const int R = 1;
 
@@ -13,21 +14,31 @@ int main(int argc, char *argv[]){
     /* Example using */
 
     /* Step 1 */
-    QImage pic = open_picture("human.png");
+    //QImage pic = open_picture("human.png");
+    QImage pic = open_picture("a_b0416f14.png");
+    if (!pic.isNull()) std::cout << "picload\n";
+    else std::cout << "pic unload";
+    gist gi;
+    QImage res = gi.build(pic);
+    save_picture(res, "gisto.png");
+    res = generate(0, 0, 5, 5, 32, 32);
+    save_picture(res, "testhaar.png");
+    ///* Step 2 */
+    //pic = lead_to_a_common_denominator(pic, R);
 
-    /* Step 2 */
-    pic = lead_to_a_common_denominator(pic, R);
+    ///* Step 3 */
+    //QImage bw_pic = make_bw_picture(pic);
+    //save_picture(bw_pic, "BW.png");
 
-    /* Step 3 */
-    QImage bw_pic = make_bw_picture(pic);
-    save_picture(bw_pic, "BW.png");
+    ///* Step 4 */
+    //LBP lbp_algorithm(bw_pic, P, R);
+    //QImage lbp_result = lbp_algorithm.create_circle_lbp();
 
-    /* Step 4 */
-    LBP lbp_algorithm(bw_pic, P, R);
-    QImage lbp_result = lbp_algorithm.create_circle_lbp();
-    
-    /* Step 5 */
-    save_picture(lbp_result, "LBP.png");
-
+    ///* Step 5 */
+    //save_picture(lbp_result, "LBP.png");
+    //gist img;
+    //img.set_n(10.0);
+    //QImage resg = img.build(pic);
+    ////img.build("LBP.png");
     return 0;
 }
